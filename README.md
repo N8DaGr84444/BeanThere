bf47ec12a23effbe7027aeee65d0797a3cefc3a0
Nathaniel Alter <nathaniel.alter@bluehalo.com>
2025-12-17 22:24:52 -0500

    initial commit, basic template

---
+++ README.md
@@ -0,0 +1,16 @@
+# BeanThere Brewed That
+BeanThere is an app to help you on your coffee journey. You can track and rank coffee shops you visit, view rankings from friends, discover new coffee shops, and more. This project is still in the very early phases.
+
+## Road Map
+- Milestone 1: Personal coffee log. Add an entry with a simple form including name of coffee shop, location, what drink you got, ranking, and a picture. View past entries and see locations on a map.
+- Milestone 2: Connect with friends. View checkings and rankings from other users. 
+- Milestone 3: Feed page. Events, specials (like holiday specials), new shops, etc.
+- Milestone 4: Map discovery and searching for more than just user entries. Advanced queries including relevancy based on personal tastes.
+- Milestone 5: AI assistant, because you can't not have AI.
+
+## Framework
+This app is built on Ionic Vue. Backend is TBD. Potentially AWS. 
+
+## How to run locally
+Must have `node` and `npm` installed.
+Clone this repo, then run `npm i` and `ionic serve` to launch in a local webpage.
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,35 +1,35 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
-import TabsPage from '../views/TabsPage.vue'
+import TabsPage from "../views/TabsPage.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: '/',
-    redirect: '/tabs/tab1'
+    path: "/",
+    redirect: "/tabs/home",
   },
   {
-    path: '/tabs/',
+    path: "/tabs/",
     component: TabsPage,
     children: [
       {
-        path: '',
-        redirect: '/tabs/tab1'
+        path: "",
+        redirect: "/tabs/home",
       },
       {
-        path: 'tab1',
-        component: () => import('@/views/Tab1Page.vue')
+        path: "home",
+        component: () => import("@/views/Home.vue"),
       },
       {
-        path: 'tab2',
-        component: () => import('@/views/Tab2Page.vue')
+        path: "log",
+        component: () => import("@/views/Log.vue"),
       },
       {
-        path: 'tab3',
-        component: () => import('@/views/Tab3Page.vue')
-      }
-    ]
-  }
-]
+        path: "profile",
+        component: () => import("@/views/Profile.vue"),
+      },
+    ],
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
